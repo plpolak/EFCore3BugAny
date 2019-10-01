766797f2a67f56696bfaf5149544f7dc63747b4c
Pieter Polak <pieter.polak@coas.com>
2019-10-01 09:02:22 +0200

    Reproduction case to EFCore3 construct with call to Enumerable.Any

---
+++ ErrorAnyEF3/DbModel/ICIREntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrorAnyEF3.DbModel
+{
+    public interface ICIREntity
+    {
+        int EntityKey { get; set; }
+        int CreatorCIRId { get; set; }
+        int MasterCIRId { get; set; }
+        bool IsActive { get; set; }
+    }
+}
---
+++ ErrorAnyEF3/DbModel/TestDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ErrorAnyEF3.DbModel
+{
+    public class TestDbContext : DbContext
+    {
+        public const string ConnectionString = "...";
+
+        public ILoggerFactory _loggerFactory;
+
+        public virtual DbSet<DbCIR> CIRs { get; set; }
+        public virtual DbSet<DbEntity> Entities { get; set; }
+        public virtual DbSet<DbEntityType> EntityTypes { get; set; }
+        public virtual DbSet<DbUserLogin> UserLogins { get; set; }
+
+        public TestDbContext(): base()
+        {
+            //_loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
+        }
+    }
+}
